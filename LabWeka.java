7688ce4782b0ebd157862b0c4f7257484b6027a8
Vitor <vitorvqz@gmail.com>
2017-04-10 23:44:57 -0300

    Atributos Ataque

--- LabWeka.java
+++ LabWeka.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package labML;
 
 import labML.Instancia.Ataque;
@@ -21,9 +16,6 @@ import weka.core.converters.ConverterUtils.DataSource;
  */
 public class LabWeka {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws Exception {
         DataSource ds = new DataSource("src/labML/DataSet/sharkattacks.arff");
         Instances ins = ds.getDataSet();        
@@ -41,6 +33,17 @@ public class LabWeka {
         Instance insAtaque = new DenseInstance(5); //Quantidade de atributos
         Ataque ataque = new Ataque();
         ataque.setInstance(insAtaque,ins);
+        
+        //ATRIBUTOS - ATAQUE
+        //@attribute tipo {NaoProvocado, Provocado, Invalido}
+        //@attribute estado {SantaCatarina, Pernambuco, RiodeJaneiro, 
+        // RioGrandeDoSul, Maranhao, Bahia, Paraiba, RioGrandeDeNorte}
+        
+        //@attribute atividade {Surfing, Swimming, Fishing, Spearfishing, Bathing, 
+        //Wading, Diving, ScubaDiving, Bodyboarding, Boogieboarding, 
+        //Windsurfing, Scientificresearch, SeaDisaster}
+        
+        //@attribute sexo {M, F}
         
         ataque.setAtaque("NaoProvocado", "Pernambuco", "Surfing", "M");
         
